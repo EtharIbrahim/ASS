5b44af3109cbefa056d63af5a645fc7b9a2e59d1
Etharibrahim <ibrahimethar0@gmail.com>
2022-05-12 06:52:34 +0500

    last

---
+++ config/dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+    // mongoURI: 'mongodb://localhost:27017/Api',
+    mongoURI: 'mongodb+srv://Ethar:MX5DugdB60qMKYdn@cluster0.ecskk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    jwtSecret: 'kdjkasjqr8239r8kfj939lwkqwlrk0239tfkeit049igkjkvjvw049t09tofjkwrjt034tigdfkjgkdfjg0rt9304fsdkfj04t904gigo'
+}
+  
